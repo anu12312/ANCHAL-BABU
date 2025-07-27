73f0b639c4ebb74e881074b30e557d10c0c2f84d
sushilbabu <premyadav109q@gmail.com>
2025-07-27 20:37:09 +0500

    Update prefix.js

--- Rudra/commands/prefix.js
+++ Rudra/commands/prefix.js
@@ -25,10 +25,10 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
     if (body === i.toUpperCase() | body === i | str === body) {
 const prefix = threadSetting.PREFIX || global.config.PREFIX;
       if (data.PREFIX == null) {
-        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n💝🥀𝐎𝐖𝐍𝐄𝐑:- ☞🆁︎🆄︎🅳︎🆁︎🅰︎ 🅹︎🅰︎🅰︎🆃︎☜ 💫\n🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 🇷‌🇺‌🇩‌🇷‌🇦‌🖤\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🤓:- ☞ www.facebook.com/🆁︎🆄︎🅳︎🆁︎🅰︎.🅹︎🅰︎🅰︎🆃︎.official\n
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @🆁︎🆄︎🅳︎🆁︎🅰︎😇`)
+        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n💝🥀𝐎𝐖𝐍𝐄𝐑:- ☞𝐒𝐔𝐒𝐇𝐈𝐋 𝐘𝐀𝐃𝐀𝐕☜ 💫\n🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 𝐒𝐔𝐒𝐇𝐈𝐋🖤\n🤍🔥𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝♥️✨:- ☞ https://www.facebook.com/share/19Ufmnhu6V/\n
+👋For Any Kind Of Help Contact On Telegram  Username 👉 @Sushil10💌`)
       }
-      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n💝🥀𝐎𝐖𝐍𝐄𝐑:- ☞Rudra jaat☜ 💫\n🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 Rudra🖤\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🤓:- ☞ www.facebook.com/Rudra.jaat.official\n👋For Any Kind Of Help Contact On Telegram  Username 👉 @Rudra😇' + data.PREFIX)
+      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n💝🥀𝐎𝐖𝐍𝐄𝐑:- ☞Ｓｕｓｈｉｌ Ｙａｄａｖ☜ 💫\n🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 Ｓｕｓｈｉｌ🖤\n🤍💥𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🩷:- ☞ https://www.facebook.com/share/19Ufmnhu6V/\n👋For Any Kind Of Help Contact On Telegram  Username 👉 @Sushil10❣️' + data.PREFIX)
     }
 
   });
